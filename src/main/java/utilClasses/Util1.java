ad658afc958b0b09600ced1d9aee205d425a16e2
Harshal <mechanicalharshal@gmail.com>
2022-10-12 23:10:44 +0530

    Action Method added to util class

--- src/main/java/utilClasses/Util1.java
+++ src/main/java/utilClasses/Util1.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -62,6 +63,11 @@ public class Util1 {
 		FileHandler.copy(source, dest);
 		return path;
 	} 
+	
+	public static void Actions(WebDriver driver, WebElement element){
+		Actions act = new Actions(driver);
+		act.moveToElement(element).perform();
+	}
 	
 	
 	
